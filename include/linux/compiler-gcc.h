72eea322027591462ad64f81c5d3c39452b713a6
Dimitris Porichis <porichisdimitris@gmail.com>
2026-06-12 21:50:20 +0300

    WIP: Testing new Dep

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -118,7 +118,11 @@
 #define __gcc_header(x) #x
 #define _gcc_header(x) __gcc_header(linux/compiler-gcc##x.h)
 #define gcc_header(x) _gcc_header(x)
-#include gcc_header(__GNUC__)
+#if __GNUC__ <= 5
+# include gcc_header(__GNUC__)
+#else
+# include <linux/compiler-gcc5.h>
+#endif
 
 #if !defined(__noclone)
 #define __noclone	/* not needed */
